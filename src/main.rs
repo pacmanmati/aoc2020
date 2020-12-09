c59e8840f935422afa80c888180dfeef0784be22
pacmanmati <pacmanmati@gmail.com>
2020-12-09 08:38:27 +0000

    part 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod one;
+
 fn main() {
-    println!("Hello, world!");
+    println!("solution to one pt 1: {}", one::solve1("one.txt").unwrap());
+    println!("solution to one pt 2: {}", one::solve2("one.txt").unwrap());
 }
--- src/one.rs
+++ src/one.rs
@@ -1,21 +1,33 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-// use std::error;
 
-// TODO: handle both types of error
+use std::fmt;
+use std::error;
 
-pub fn solve(filepath: &str) -> Result<u32> {
-    let file = File::open(filepath)?;
-    let file = BufReader::new(file);
+type MyResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-    let mut vec = Vec::new();
-    
-    for line in file.lines() {
-	vec.push(line?.parse::<u32>()?);
+#[derive(Debug)]
+struct NoMatch;
+
+impl fmt::Display for NoMatch {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "no match could be found")
     }
+}
 
-    let mut result: Option<u32> = None;
+impl error::Error for NoMatch {} // ?
+
+// TODO: fix the return type
+fn read(filepath: &str) -> MyResult<Vec<i32>> {
+    // unholy abomination
+    BufReader::new(File::open(filepath)?).lines().map(|line| line.unwrap()).map(|line| line.parse::<i32>().unwrap()).collect()
+}
+
+pub fn solve1(filepath: &str) -> MyResult<i32> {
+    let vec = read(filepath)?;
+    // println!("{:?}", vec);
+    let mut result: Option<i32> = None;
     for fst in vec.iter() {
 	let x = 2020 - fst;
 	for scd in vec.iter() {
